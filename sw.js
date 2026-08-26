c44575c5f0083319f7c8e6e855ed19377bb295d9
Tluzzi450 <tluzzi450@alumnos.frh.utn.edu.ar>
2026-08-26 10:41:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  *
  * Al publicar una versión nueva: cambiá VERSION. Eso descarta la caché vieja.
  */
-const VERSION = "2026.09.03";
+const VERSION = "2026.09.04";
 const CACHE = "planificador-industrial-" + VERSION;
 
 const ESENCIALES = [
